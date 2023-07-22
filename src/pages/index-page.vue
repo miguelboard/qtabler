753b1fe67790612b8c554d12d92b7c1972f2968d
Miguel <85888237+imkhoat@users.noreply.github.com>
2023-07-22 16:27:07 +0700

    feat: implement t-window component (#6)

--- src/pages/index-page.vue
+++ src/pages/index-page.vue
@@ -1,3 +1,39 @@
 <template>
-  <q-page class="row items-center justify-evenly"> </q-page>
+  <q-page class="row items-center justify-evenly">
+    <t-window
+      name="window_panel"
+      title="Hello"
+      sub-title="Welcome to window feature!"
+      class="window_panel"
+    >
+      <q-list bordered separator class="rounded-borders">
+        <q-item clickable v-ripple>
+          <q-item-section>Single line item</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section>
+            <q-item-label>Item with caption</q-item-label>
+            <q-item-label caption>Caption</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section>
+            <q-item-label overline>OVERLINE</q-item-label>
+            <q-item-label>Item with caption</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+      <template v-slot:footer>
+        <q-space></q-space>
+        <q-btn unelevated color="primary" class="q-px-lg">Save</q-btn>
+      </template>
+    </t-window>
+  </q-page>
 </template>
+<style lang="scss" setup>
+.window_panel {
+  min-width: 500px;
+}
+</style>
